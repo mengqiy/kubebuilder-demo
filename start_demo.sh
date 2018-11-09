28477af64b57075c7c43af45c677346f67d74984
Mengqi Yu <mengqiyu1991@gmail.com>
2018-11-09 14:47:32 -0800

    better printing

--- start_demo.sh
+++ start_demo.sh
@@ -31,34 +31,60 @@ DEMO_PROMPT="$ "
 
 #clear
 
+p "====> [ Scaffold the project structure ]"
 pe "kubebuilder init --domain=cncf.io --dep=false"
+p ""
 
+p "====> [ Scaffold the CRD API and controller]"
 pe "kubebuilder create api --group=example --version=v1alpha1 --kind=BoundedDeployment --resource --controller --make=false"
+p ""
 
+p "====> [ Scaffold the webhook]"
 pe "kubebuilder alpha webhook --group=example --version=v1alpha1 --kind=BoundedDeployment --type=mutating --operations=create,update --make=false"
+p ""
 
+p "====> [ Run make ]"
 pe "make"
+p ""
 
+p "====> [ commit generated code ]"
 pe "git add -A"
 pe "git commit -m 'init commit'"
+p ""
 
+p "====> [ add business logic ]"
 pe "git apply demo/business_logic.patch"
+p ""
 
+p "====> [ Show what user needs to implement ]"
 pe "git diff"
+pe "git commit -am 'add business logic'"
+p ""
 
+p "====> [ Run make again to update generated code ]"
 pe "make"
+p ""
 
+p "====> [ Show generated code has been updated ]"
 pe "git diff"
+p ""
 
 # changes the yaml of a CR instance and create yaml for another instance.
+p "====> [ Update CR instances ]"
 pe "git apply demo/CR.patch"
 # use the image built locally for minikube
 pe "git apply demo/image_pull.patch"
+p ""
 
+p "====> [ Build docker image ]"
 pe "make docker-build"
+p ""
 
+p "====> [ Deploy the controller and webhook to cluster ]"
 pe "make deploy"
+p ""
 
+p "====> [ Verify the controller and webhooks work ]"
 pe "cat config/samples/example_v1alpha1_boundeddeployment.yaml | grep replicas"
 pe "kubectl apply -f config/samples/example_v1alpha1_boundeddeployment.yaml"
 pe "kubectl get deployments boundeddeployment-sample-deployment -o yaml | grep replicas"
